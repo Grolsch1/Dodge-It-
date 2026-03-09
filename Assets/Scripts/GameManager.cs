fe4fc344af88f9c41f15422ade65599f6c1edda2
Grolsch1 <chrisbeckenstrater@gmail.com>
2026-03-09 17:20:32 +0200

    Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,66 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager instance;
+
+    public GameObject pauseMenu;
+    public GameObject deathScreen;
+    public GameObject startMenu;
+
+    bool isPaused = false;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Time.timeScale = 0f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void StartGame()
+    {
+        startMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void TogglePause()
+    {
+        if (deathScreen.activeSelf || startMenu.activeSelf)
+            return;
+
+        isPaused = !isPaused;
+
+        pauseMenu.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+
+    public void PlayerDied()
+    {
+        deathScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
